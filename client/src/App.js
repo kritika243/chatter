5dc36fdd5d9af51e6d80ff97cf7b606c741c83e3
kritika243 <24kritikasharma24@gmail.com>
2021-10-27 18:29:51 +0530

    socket.io connection on client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
+import socketIO from 'socket.io-client'
 import './App.css'
 
+const ENDPOINT = 'http://localhost:4500/'
+const socket = socketIO(ENDPOINT, { transports: ['websocket'] })
+
 function App() {
+  socket.on('connect', () => {})
   return (
     <div className='App'>
       <h1>Working</h1>
