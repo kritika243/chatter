fcf907105cdd19f8dae2f433d2436afd777dbfb4
kritika243 <24kritikasharma24@gmail.com>
2021-10-27 17:47:52 +0530

    starting the server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,19 @@
 const http = require('http')
+const express = require('express')
+const cors = require('cors')
+const socketIO = require('socket.io')
+
+const app = express()
+const port = 4500 || process.env.port
+
+app.get('/', (req, res) => {
+  res.send('its working')
+})
+
+const server = http.createServer(app)
+
+const io = socketIO(server)
+
+server.listen(port, () => {
+  console.log(`server is working on ${port}`)
+})
