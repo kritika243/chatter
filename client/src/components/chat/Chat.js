659c9d1eb2499cca0c3d9f86cd0a415028b81ce9
kritika243 <24kritikasharma24@gmail.com>
2021-11-01 13:50:48 +0530

    chat component and new server

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { user } from '../join/Join'
+import socketIO from 'socket.io-client'
+
+const ENDPOINT = 'http://localhost:4500/'
 
 const Chat = () => {
+  const socket = socketIO(ENDPOINT, { transports: ['websocket'] })
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      alert('connected')
+    })
+    return () => {}
+  }, [socket])
+
   return (
-    <div>
-      <h1>{user}</h1>
+    <div className='chatPage'>
+      <div className='chatContainer'>
+        <div className='chatHeader'></div>
+        <div className='chatBox'></div>
+        <div className='inputBox'></div>
+      </div>
     </div>
   )
 }
